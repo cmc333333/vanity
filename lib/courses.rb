d3434a59f525532b29e933c5903a3b8fe43dca63
CM Lubinski <cmc@agile>
2013-04-13 01:35:32 -0500

    Course list

--- lib/courses.rb
+++ lib/courses.rb
@@ -1,14 +1,19 @@
+include Nanoc::Helpers::HTMLEscape
 include Nanoc::Helpers::Rendering
 require 'json'
 
 
 def course_list
   fields = ["title", "start", "end", "professor", "professor's university", 
-    "categories", "languages", "libraries", "syllabus"]
+    "university", "categories", "languages", "libraries", "syllabus", "id"]
 
-  items.select {|i| not i.binary? and i[:meta_filename] and
-    i[:meta_filename].match(/content\/data\/courses\/.*\.yaml/)
-  }.sort_by {|i| i[:end]}.reverse.map{ |i| i.attributes }.map{|i|
+  items.select {|i| 
+    not i.binary? and i[:meta_filename] and i[:meta_filename].match(/content\/data\/courses\/.*\.yaml/)
+  }.sort_by {|i| 
+    i[:end]
+  }.reverse.map{|i| 
+    i.attributes.merge({:id => i[:meta_filename].match(/content\/data\/courses\/(.*)\.yaml/)[1]})
+  }.map{|i|
     i.reject { |k,v| ! fields.include? k.to_s } # reject gives us a Hash
   }
 end
@@ -16,7 +21,7 @@ end
 def courses
   items << Nanoc::Item.new(
     "<%= render 'courses' %>",
-    {:title => "Courses"},
+    {:title => "Courses", :extra_css => ["/assets/css/courses.css"]},
     "/cv/courses/"
   )
 end
