82c7fdaac4b760f2a1ced67bec913b787e7ae669
CM Lubinski <cmc@agile>
2013-04-12 23:08:35 -0500

    Classes as JSON

--- lib/courses.rb
+++ lib/courses.rb
@@ -1,9 +1,16 @@
 include Nanoc::Helpers::Rendering
+require 'json'
+
 
 def course_list
+  fields = ["title", "start", "end", "professor", "professor's university", 
+    "categories", "languages", "libraries", "syllabus"]
+
   items.select {|i| not i.binary? and i[:meta_filename] and
     i[:meta_filename].match(/content\/data\/courses\/.*\.yaml/)
-  }.sort_by {|i| i[:end]}.reverse
+  }.sort_by {|i| i[:end]}.reverse.map{ |i| i.attributes }.map{|i|
+    i.reject { |k,v| ! fields.include? k.to_s } # reject gives us a Hash
+  }
 end
 
 def courses
