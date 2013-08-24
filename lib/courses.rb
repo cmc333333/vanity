85c7180089c37a3573851ed2ced929770c963d64
CM Lubinski <cmc@agile>
2013-08-24 16:52:11 -0500

    Courses by year

--- lib/courses.rb
+++ lib/courses.rb
@@ -1,6 +1,7 @@
 include Nanoc::Helpers::HTMLEscape
 include Nanoc::Helpers::Rendering
 require 'json'
+require 'set'
 
 
 def course_list
@@ -8,13 +9,28 @@ def course_list
     "university", "categories", "languages", "libraries", "syllabus", "id"]
 
   items.select {|i| 
-    not i.binary? and i[:meta_filename] and i[:meta_filename].match(/content\/data\/courses\/.*\.yaml/)
-  }.sort_by {|i| 
-    i[:end]
-  }.reverse.map{|i| 
-    i.attributes.merge({:id => i[:meta_filename].match(/content\/data\/courses\/(.*)\.yaml/)[1]})
-  }.map{|i|
-    i.reject { |k,v| ! fields.include? k.to_s } # reject gives us a Hash
+    not i.binary? \
+    and i[:meta_filename] \
+    and i[:meta_filename].match(/content\/data\/courses\/.*\.yaml/)
+  }.group_by {|i| 
+    i[:start].year
+  }.to_a.sort_by{|year, courses|
+    year
+  }.reverse.map{|year, courses|
+    courses = courses.sort_by{|i|
+      i[:start]
+    }.reverse.map{|i| 
+      i.attributes.merge({:id => 
+        i[:meta_filename].match(/content\/data\/courses\/(.*)\.yaml/)[1]})
+    }.map{|i|
+      # Remove any fields we don't want
+      i.reject { |k,v| ! fields.include? k.to_s } # reject gives us a Hash
+    }
+    universities = Set.new()
+    courses.each{|course|
+      universities.add(course[:university])
+    }
+    [year, universities.to_a.sort, courses]
   }
 end
 
