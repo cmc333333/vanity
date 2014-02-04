b11609d64d9144c377d2a1c030c64e53713ce853
CM Lubinski <cmc@agile>
2014-02-03 22:48:06 -0600

    Link to certs

--- lib/courses.rb
+++ lib/courses.rb
@@ -3,6 +3,25 @@ include Nanoc::Helpers::Rendering
 require 'json'
 require 'set'
 
+def add_certs
+  certs = items.select {|i|
+    i[:filename] \
+    and i[:filename].start_with? "content/assets/files/certificates"
+  }.map{|i|
+    [i.trimmed_filename, i]
+  }
+  certs = Hash[certs]
+
+  items.select {|i|
+    not i.binary? \
+    and i[:filename] \
+    and i[:filename].start_with? "content/courses" \
+    and certs.include? i.trimmed_filename
+  }.each {|i|
+    i[:cert] = certs[i.trimmed_filename]
+  }
+end
+
 def course_markdown
   items.select {|i|
     not i.binary? \
--- lib/default.rb
+++ lib/default.rb
@@ -1,2 +1,15 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
+
+class Nanoc::Item
+  def trimmed_filename(include_ext=false)
+    filename = @attributes[:filename]
+    if not filename.nil?
+      filename = filename.split("/")[-1]
+      if not include_ext
+        filename = filename.split(".")[0]
+      end
+    end
+    filename
+  end
+end
