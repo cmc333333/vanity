e4eee1f9758594fe798079caee87ca4329cac109
CM Lubinski <cm.lubinski@gmail.com>
2017-12-26 00:21:09 -0500

    Use plugin for analytics tracking.

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,9 +10,13 @@ module.exports = {
     'gatsby-source-goodreads',
     'gatsby-source-podcasts',
     'gatsby-transformer-remark',
+    'gatsby-transformer-yaml',
+    'gatsby-plugin-nprogress',
     {
-      resolve: 'gatsby-plugin-nprogress',
-      options: {},
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-87597611-1',
+      },
     },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -129,16 +129,6 @@ export default function Layout(props) {
           <span>&copy; C.M. Lubinski 2008-2017</span>
         </div>
       </div>
-      <script type="text/javascript">
-        {`
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-          ga('create', 'UA-87597611-1', 'auto');
-          ga('send', 'pageview');
-        `}
-      </script>
     </div>
   );
 }
