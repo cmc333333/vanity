a3d8587ee79f753217dd32eddf6255ecb679c61f
CM Lubinski <cm.lubinski@gmail.com>
2019-12-30 16:16:57 -0500

    Add doubles plugin for local development.

---
+++ plugins/gatsby-source-doubles/gatsby-node.js
@@ -0,0 +1,58 @@
+const faker = require('faker');
+const { createRemoteFileNode } = require('gatsby-source-filesystem');
+const pIter = require('p-iteration');
+
+const createRandomImage = async (sourceNodesArgs) => {
+  const {
+    actions: { createNode },
+    cache,
+    createNodeId,
+    store,
+  } = sourceNodesArgs;
+  const fileNode = await createRemoteFileNode({
+    createNode,
+    createNodeId,
+    store,
+    cache,
+    url: faker.random.image(),
+  });
+  return fileNode.id;
+};
+
+const createDouble = async (schema, sourceNodesArgs) => {
+  if (!schema) {
+    return schema;
+  } else if (typeof schema === 'string') {
+    const [nameSpace, methodName] = schema.split('.');
+    return faker[nameSpace][methodName]();
+  } else if (Array.isArray(schema)) {
+    return pIter.map(schema, s => createDouble(s, sourceNodesArgs));
+  }
+
+  const result = {};
+  await pIter.forEach(Object.entries(schema), async ([fieldName, fieldType]) => {
+    if (fieldType === 'image') {
+      result[`${fieldName}___NODE`] = await createRandomImage(sourceNodesArgs);
+    } else {
+      result[fieldName] = await createDouble(fieldType, sourceNodesArgs);
+    }
+  });
+  return result;
+};
+
+exports.sourceNodes = async (sourceNodesArgs, pluginOptions) => {
+  const { actions: { createNode }, createContentDigest, createNodeId } = sourceNodesArgs;
+  const { schema, count, type } = pluginOptions;
+  await pIter.forEach(Array.from(Array(count).keys()), async () => {
+    const double = await createDouble(schema, sourceNodesArgs);
+    await createNode({
+      ...double,
+      id: createNodeId(faker.random.uuid()),
+      parent: null,
+      internal: {
+        type,
+        contentDigest: createContentDigest(double),
+      },
+    });
+  });
+};
